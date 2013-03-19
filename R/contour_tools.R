8a5204ed92109fa55f2120ed2e77213186a360c6
Joshua French <joshua.french@gmail.com>
2013-03-19 15:48:46 -0600

    0.5.0 Beta 1

--- R/contour_tools.R
+++ R/contour_tools.R
@@ -1,18 +1,25 @@
 #Take the list of contours from contourLines() and extracts coordinates 
-get.contours=function(contours.list)
+get.contours=function(x)
 {
-	x <- y <- NULL
-	for(i in 1:length(contours.list))
+	x1 <- x2 <- NULL
+	for(i in 1:length(x))
 	{
-		x <- c(x, contours.list[[i]]$x)
-		y <- c(y, contours.list[[i]]$y)
+		x1 <- c(x1, x[[i]]$x)
+		x2 <- c(x2, x[[i]]$y)
 	}
-	cbind(x,y)
+	cbind(x = x1,y = x2)
 }
 
 #Plot contour lines from contourLines
-plot.contourLines <- function(x, begin=1, end = length(x), ...)
+plot.contourLines <- function(x, begin=1, end = length(x), add = FALSE, ...)
 {
+	if(!add)
+	{
+		contours <- get.contours(x)
+		rx <- range(contours[,1])
+		ry <- range(contours[,2])
+		plot(rx, ry, type = "n", ...)
+	}
 	for(i in begin:end)
 	{
 		lines(x[[i]]$x, x[[i]]$y, ...)
