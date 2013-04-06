b21efffbafd62373898de6003d435135d39dc584
Joshua French <joshua.french@gmail.com>
2013-04-06 09:29:33 -0600

    0.5.1 alpha 2

--- R/krige_tools.R
+++ R/krige_tools.R
@@ -1,3 +1,66 @@
+krige.uk2 <- function(y, V, Vp, Vop, X, Xp, ...)
+{
+	# Check arguments. Create unspecified arguments if needed.
+	nsim <- 0; Ve.diag <- NULL; method <- "eigen"; level <- NULL; alternative <- NULL
+	arglist <- list(...)
+	argnames <- names(arglist)
+	if("nsim" %in% argnames){ nsim <- arglist$nsim }
+	if("Ve.diag" %in% argnames){ Ve.diag <- arglist$Ve.diag }
+	if("method" %in% argnames){ method <- arglist$method }
+	if("level" %in% argnames){ level <- arglist$level }
+	if("alternative" %in% argnames){ alternative <- arglist$alternative }
+	ins <- krige_arg_check(y, V, Vp, Vop, X, Xp, m = 0, nsim, Ve.diag, method,
+	level, alternative)
+
+	###compute matrix products for future use
+	ViX <- solve(V, X)
+	XtViX <- crossprod(ViX, X)
+
+	#compute gls estimates of regression coefficients
+	coeff <- solve(XtViX, crossprod(ViX, y))
+
+	#compute kriging weights
+	w <- solve(V, Vop - X%*%solve(XtViX, crossprod(X, solve(V, Vop)) - t(Xp)))
+
+	#blup for yp
+	pred <- crossprod(w, y)
+
+	#variance of (yp - pred)
+	mspe <- colSums((V %*% w) * w) - 2 * colSums(w * Vop) + diag(Vp)
+
+	out <- list(pred = pred, mspe = mspe, coeff = coeff,
+	vcov.coef = solve(XtViX))
+
+	# generate conditional realizations if nsim > 0
+	if(nsim > 0)
+	{
+		# determine number of observed and predicted data values
+		n <- nrow(V); np <- nrow(Vp)
+
+		# modify V to account for error
+		Vomod <- V - diag(Ve.diag)
+
+		# Combine observed and predicted covariance matrices
+		Va <- rbind(cbind(Vomod, Vop), cbind(t(Vop), Vp))
+
+		# Generate mean zero normal random variables with appropriate covariance
+		# matrix (does not include error)
+		newsim <- decomp.cov(Va, method = method) %*%
+		matrix(rnorm(nrow(Va) * nsim), ncol = nsim)
+
+		# Take difference of observed data and simulated observations
+		# (now including error)
+		newZ <- matrix(y, nrow = n, ncol = nsim) - newsim[1:n,] +
+		matrix(rnorm(n * nsim, sd = sqrt(Ve.diag)), nrow = n, ncol = nsim)
+
+		# Create conditional realizations
+		sim <- newsim[-(1:n),] + crossprod(w, newZ)
+
+		out$sim <- sim
+	}
+	return(out)
+}
+
 krige.uk <- function(y, V, Vp, Vop, X, Xp, ...)
 {
 	# Check arguments.  Create unspecified arguments if needed.
@@ -9,27 +72,22 @@ krige.uk <- function(y, V, Vp, Vop, X, Xp, ...)
 	if("method" %in% argnames){ method <- arglist$method }
 	if("level" %in% argnames){ level <- arglist$level }
 	if("alternative" %in% argnames){ alternative <- arglist$alternative }
-	ins <- krige_arg_check(y, V, Vp, Vop, X, Xp, m = 0, nsim, Ve.diag, method, 
-		level, alternative)
 
 	###compute matrix products for future use
 	ViX <- solve(V, X)
 	XtViX <- crossprod(ViX, X)
-	
+	ViVop <- solve(V, Vop)
+
 	#compute gls estimates of regression coefficients
 	coeff <- solve(XtViX, crossprod(ViX, y))
 
-	#compute kriging weights
-	w <- solve(V, Vop - X%*%solve(XtViX, crossprod(X, solve(V, Vop)) - t(Xp)))
+	pred <- Xp %*% coeff + crossprod(ViVop, y - X %*% coeff)
 
-	#blup for yp
-	pred <- crossprod(w, y)
-	
-	#variance of (yp - pred)
-	mspe <- colSums((V %*% w) * w) - 2 * colSums(w * Vop) + diag(Vp)
+	A <- Xp - crossprod(ViVop, X)
 
-	out <- list(pred = pred, mspe = mspe, coeff = coeff, 
-		vcov.coef = solve(XtViX))
+	mspe <- diag(Vp) - colSums(Vop * ViVop) + colSums(t(A) * solve(XtViX, t(A)))
+
+	out <- list(pred = pred, mspe = mspe, coeff = coeff, vcov.coef = solve(XtViX))
 
 	# generate conditional realizations if nsim > 0
 	if(nsim > 0)
@@ -48,20 +106,24 @@ krige.uk <- function(y, V, Vp, Vop, X, Xp, ...)
 		newsim <- decomp.cov(Va, method = method) %*% 
 			matrix(rnorm(nrow(Va) * nsim), ncol = nsim)
 	
-		# Take difference of observed data and simulated observations 
-		# (now including error)
-		newZ <- matrix(y, nrow = n, ncol = nsim) - newsim[1:n,] + 
+		# Generate synthetic data (including error)
+		newZ <- newsim[1:n,] + 
 			matrix(rnorm(n * nsim, sd = sqrt(Ve.diag)), nrow = n, ncol = nsim)
-	
-		# Create conditional realizations
-		sim <- newsim[-(1:n),] + crossprod(w, newZ)
+
+		# Compute gls estimates of regression coefficients for synthetic data
+		coeff.newZ <- solve(XtViX, crossprod(ViX, newZ))
 		
+		# Compute conditional realizations.  The second and third terms simulate
+		# the kriging error.
+		sim <- matrix(pred, nrow = nrow(Vp), ncol = nsim) + newsim[-(1:n),] - 
+			Xp %*% coeff.newZ - crossprod(ViVop, newZ - X %*% coeff.newZ)
+			
 		out$sim <- sim
 	}
 	return(out)
 }
 
-krige.ok <- function(y, V, Vp, Vop, ...)
+krige.ok2 <- function(y, V, Vp, Vop, ...)
 {
 	# Check arguments.  Create unspecified arguments if needed.
 	nsim <- 0; Ve.diag <- NULL; method <- "eigen"; level <- NULL; alternative <- NULL
@@ -85,6 +147,76 @@ krige.ok <- function(y, V, Vp, Vop, ...)
 	out$coeff <- as.vector(out$coeff)
 	return(out)
 }
+
+krige.ok <- function(y, V, Vp, Vop, ...)
+{
+	# Check arguments.  Create unspecified arguments if needed.
+	nsim <- 0; Ve.diag <- NULL; method <- "eigen"; level <- NULL; alternative <- NULL
+	arglist <- list(...)
+	argnames <- names(arglist)
+	if("nsim" %in% argnames){ nsim <- arglist$nsim }
+	if("Ve.diag" %in% argnames){ Ve.diag <- arglist$Ve.diag }
+	if("method" %in% argnames){ method <- arglist$method }
+	if("level" %in% argnames){ level <- arglist$level }
+	if("alternative" %in% argnames){ alternative <- arglist$alternative }
+	ins <- krige_arg_check(y, V, Vp, Vop, X = NULL, Xp = NULL, m = 0, nsim, Ve.diag, method, 
+		level, alternative)
+
+	# Define covariates matrices
+	X <- matrix(1, nrow = nrow(V))
+	Xp <- matrix(1, nrow = nrow(Vp))
+
+	###compute matrix products for future use
+	ViX <- solve(V, X)
+	XtViX <- crossprod(ViX, X)
+	ViVop <- solve(V, Vop)
+	A <- Xp - crossprod(ViVop, X)
+
+	#compute gls estimates of regression coefficient (in a fancy way to optimize speed)
+	coeff <- as.vector(crossprod(ViX, y)/XtViX[1,1])
+
+	# compute predictions and mspe
+	pred <- coeff + crossprod(ViVop, y - coeff)
+	mspe <- diag(Vp) - colSums(Vop * ViVop) + A^2/XtViX[1,1]
+
+	out <- list(pred = pred, mspe = mspe, coeff = coeff, vcov.coef = solve(XtViX))
+
+	# generate conditional realizations if nsim > 0
+	if(nsim > 0)
+	{
+		# determine number of observed and predicted data values
+		n <- nrow(V); np <- nrow(Vp)
+
+		# modify V to account for error
+		Vomod <- V - diag(Ve.diag)
+		
+		# Combine observed and predicted covariance matrices
+		Va <- rbind(cbind(Vomod, Vop), cbind(t(Vop), Vp))
+	
+		# Generate mean zero normal random variables with appropriate covariance
+		# matrix (does not include error)
+		newsim <- decomp.cov(Va, method = method) %*% 
+			matrix(rnorm(nrow(Va) * nsim), ncol = nsim)
+	
+		# Generate synthetic data (including error)
+		newZ <- newsim[1:n,] + 
+			matrix(rnorm(n * nsim, sd = sqrt(Ve.diag)), nrow = n, ncol = nsim)
+
+		# Compute gls estimates of regression coefficients for synthetic data
+		coeff.newZ <- solve(XtViX, crossprod(ViX, newZ))
+		
+		# Compute conditional realizations.  The second and third terms simulate
+		# the kriging error.
+		sim <- matrix(pred, nrow = nrow(Vp), ncol = nsim) + newsim[-(1:n),] - 
+			Xp %*% coeff.newZ + crossprod(ViVop, newZ - X %*% coeff.newZ)
+			
+#sim2 <- matrix(pred, nrow = nrow(Vp), ncol = nsim) + newsim[-(1:n),] - 
+#			coeff.newZ[1,1] - crossprod(ViVop, newZ - coeff.newZ[1,1])			
+		out$sim <- sim
+	}
+	return(out)
+}
+
 
 krige.sk <- function(y, V, Vp, Vop, m = 0, ...)
 {
