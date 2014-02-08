e402408e9f7fbf155767376541c384cbc84f0d60
jpfrench81 <joshua.french@gmail.com>
2014-02-07 17:05:00 -0700

    Version 0.5.6
    Updated preamble, DESCRIPTION to handle updated Rcpp.

--- R/preamble.R
+++ R/preamble.R
@@ -3,7 +3,7 @@
     x <- regexpr("[0-9]{4}", date)
     yr <- substr(date, x[1], x[1] + attr(x, "match.length") - 1)
 
-	greet <- paste("#", "# SpatialTools Package v.0.5.4", "# 2013-08-06", 
+	greet <- paste("#", "# SpatialTools Package v.0.5.6", "# 2014-01-28", 
 	paste("# Copyright (C) 2011-", yr, ", Joshua P. French", sep = ""), 
 	"# Written by Joshua P. French", 
 	"# This research was partially supported under NSF Grant ATM-0534173", "#", sep = "\n")
